ed9a469c7e5804e767c524be8167b8f4ba8f7262
Juan Garay <juannchin@users.noreply.github.com>
2021-07-02 13:43:46 -0600

    Create prueba.php

---
+++ prueba.php
@@ -0,0 +1,13 @@
+<?php
+class ClaseSencilla
+{
+    // Declaración de una propiedad
+    public $var = 'un valor predeterminado';
+
+    // Declaración de un método
+    public function mostrarVar() {
+        echo $this->var;
+    }
+}
+
+?>
